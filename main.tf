a58f5453e0369f0acf49b84b98adc6f0c97d960c
Ioannis Moustakis <moustakisioannis@gmail.com>
2026-08-19 14:33:17 +0200

    Refactor VPC and EKS configuration with modules
    Replaced data sources for VPC and subnets with module outputs. Added VPC module configuration and updated EKS module to use the new VPC.

--- main.tf
+++ main.tf
@@ -14,23 +14,31 @@ resource "random_string" "suffix" {
   special = false
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+data "aws_availability_zones" "available" {
+    filter {
+      name   = "opt-in-status"
+      values = ["opt-in-not-required"]
+    }
   }
-}
 
-resource "aws_ec2_tag" "elb_role" {
-  for_each    = toset(data.aws_subnets.default.ids)
-  resource_id = each.value
-  key         = "kubernetes.io/role/elb"
-  value       = "1"
-}
+  module "vpc" {
+    source  = "terraform-aws-modules/vpc/aws"
+    version = "~> 5.8"
+
+    name = "education-vpc"
+    cidr = "10.0.0.0/16"
+    azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+    enable_nat_gateway   = true
+    single_nat_gateway   = true
+    enable_dns_hostnames = true
+
+    public_subnet_tags  = { "kubernetes.io/role/elb" = 1 }
+    private_subnet_tags = { "kubernetes.io/role/internal-elb" = 1 }
+  }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -48,8 +56,8 @@ module "eks" {
     }
   }
 
-  vpc_id     = data.aws_vpc.default.id
-  subnet_ids = data.aws_subnets.default.ids
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2023_x86_64_STANDARD"
